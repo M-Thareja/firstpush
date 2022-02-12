f7336b99c676f72a32f247bae2e318a53f7fff95
m-thareja <mannanthareja@gmail.com>
2022-02-12 13:30:16 +0530

    Init Commit

---
+++ public/js/app.js
@@ -0,0 +1,47 @@
+console.log('Client side javascript file is loaded!')
+
+// fetch('http://localhost:3000/weather?address=hawaii').then((response)=>{
+//     // if(error){
+//     //    return console.log("Can't connect to the server.")
+//     //  }
+//     console.log(response);
+
+//      response.json().then((data)=>{
+//         if(data.error){
+//             console.log(error)
+//         }
+//         else{
+//             console.log(data.temp)
+//         }
+//     })
+// })
+
+
+const form = document.querySelector('form');
+const inputdata = document.querySelector('input');
+const message1 = document.querySelector('#message1')
+const message2= document.querySelector('#message2')
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+    const location = inputdata.value;
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response)=>{
+
+
+     response.json().then((data)=>{
+        if(data.error){
+            console.log(data.error)
+            message1.textContent = data.error;
+        }
+        else{
+            console.log(data.temp)
+            message1.textContent = `The Temperature of ${location} is ${data.temp} celcius.`
+            message2.textContent = `It can be ${data.discription}`
+        }
+    })
+})
+
+})
+
---
+++ src/utils/geo.js
@@ -0,0 +1,56 @@
+const { url } = require("inspector");
+const request = require("request");
+
+
+
+
+const forecast = (lat, lon, callback)=>{
+
+    const url = `http://api.weatherstack.com/current?access_key=40be3f64a20ca4e67bc0240fa0ab5fc7&query=${lat},${lon}`
+
+    request({url : url, json : true}, (error, response)=>{
+        if(error){
+            callback("Server Error Can't Connect.",undefined);
+        }
+        else if(response.body.error){
+            callback("Wrong Information", undefined);
+        }
+        else{
+            callback(undefined, {
+                temp : response.body.current.temperature,
+                region : response.body.location.region,
+                desc : response.body.current.weather_descriptions
+
+            })
+        }
+    })
+
+}
+
+const geocode = (address, callback)=>{
+
+    const geourl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1IjoiY3VzdG1yNTU1MzU3IiwiYSI6ImNrdnJpbmNoMjA4Nmcyb205MGRrejFrNDQifQ.QTAPRhDrNyuN-zBae4E6Sg&limit=1`
+
+    request({url : geourl, json: true}, (error, response)=>{
+        if(error){
+            callback("Servier Not Found.", undefined);
+        }
+        else if(response.body.features.length == 0){
+            callback("Location Not Found.", undefined);
+        }
+        else{
+            // const lat = body.features[0].center[1]
+            // const lon = body.features[0].center[0]
+            callback(undefined,{
+                lat :response.body.features[0].center[1],
+                lon :response.body.features[0].center[0]
+            })
+        }
+    })
+    
+}
+
+module.exports = {
+    geocode : geocode,
+    forecast : forecast
+}
